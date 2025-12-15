06fd502d0d24783d5428e97987afd7fbde86c8e7
Angela-Fl <agfleenor@gmail.com>
2025-12-15 10:51:19 -0700

    test: Add placeholder tests for empty helpers

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  # Currently no helper methods to test
+  # This is a placeholder for future helper methods
+
+  test "helper module exists" do
+    assert_nothing_raised do
+      ApplicationHelper
+    end
+  end
+end
---
+++ test/helpers/settings_helper_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class SettingsHelperTest < ActionView::TestCase
+  # Currently no helper methods to test
+  # This is a placeholder for future helper methods
+
+  test "helper module exists" do
+    assert_nothing_raised do
+      SettingsHelper
+    end
+  end
+end
