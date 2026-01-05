2819ae530c7cf980a173c073031ad49704e1127c
Angela-Fl <agfleenor@gmail.com>
2026-01-04 19:55:51 -0700

    create getting started page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  # Allow public access to getting started page
+  skip_before_action :authenticate_user!, only: [ :getting_started ]
+
+  def getting_started
+    # Public page - no authentication required
+  end
+
+  def feedback
+    # Requires authentication (default behavior)
+  end
+end
